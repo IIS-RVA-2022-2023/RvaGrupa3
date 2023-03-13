51062a26a5fad5ebe72c3313861e1a37371065de
vf98 <vf98@AR1-Asistent.ftn.ns>
2023-03-13 10:03:11 +0100

    Trece vezbe 13.03.2023.

---
+++ RvaBackEndG3/src/main/java/rva/repository/ArtiklRepository.java
@@ -0,0 +1,12 @@
+package rva.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import rva.model.Artikl;
+
+public interface ArtiklRepository extends JpaRepository<Artikl,Long> {
+
+	List<Artikl> findByNazivContainingIgnoreCase(String naziv);
+}
